09509a645482af3db54818e93e886277fac0c5e7
JacksonDAlbini <149831221+JacksonDAlbini@users.noreply.github.com>
2025-02-11 11:59:44 -0700

    Add files via upload

---
+++ adventureGame copy.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.io.File;
+public class adventureGame{
+    public static void main (String[] args) {
+        Scanner scan = new Scanner(System.in);
+        File room = new File("room.txt");
+        scan = new Scanner (room1);
+        while (scan.hasNextLine()){
+
+        }
+    }
+}
